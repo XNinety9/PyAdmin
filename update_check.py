c53d6cea2dbd3f2450665d660a453dd92b1c3bd2
deadbird <deadbird99@gmail.com>
2014-05-12 18:11:25 +0200

    Removed comments.

--- update_check.py
+++ update_check.py
@@ -13,15 +13,15 @@ str = p.stderr.read();
 updates = int(str.split(";")[0])
 sec_updates = int(str.split(";")[1]) 
 
-print "Number of packets that can be updated: {0}".format(updates)
-print "Number of security update: {0}".format(sec_updates)
+#print "Number of packets that can be updated: {0}".format(updates)
+#print "Number of security update: {0}".format(sec_updates)
 
 #if result is non zero, tweet it
 if updates != 0 or sec_updates != 0:
-	print "Twitting..."
+	#print "Twitting..."
 	cur_date = time.strftime("%x")
 	cur_time = time.strftime("%X")
 	tweet = "{0} - {1} - Your server has {2}+{3} updates pending!".format(cur_date, cur_time, updates, sec_updates)
 	p = subprocess.Popen("/usr/local/bin/twitter -edeadbird.fr@gmail.com set {0}".format(tweet), stderr=subprocess.PIPE, shell=True)
-	out, err = p.communicate()
-	print "Twitt sent!"
+	#out, err = p.communicate()
+	#print "Twitt sent!"
