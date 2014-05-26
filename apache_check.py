ee743ffd41f8bc6ab3a3a963bcbb8eaec5727cf6
deadbird <deadbird99@gmail.com>
2014-05-26 14:45:15 +0200

    Changes
    - Changed update-check command. I now use apt-get update -s (for
    « simulate ») to check for updates. This one works much better as
    apt-check is depreciated.
    - Created apache-check. This one alerts me if apaches has stopped
    running.

---
+++ apache_check.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*-
+
+import subprocess
+import time
+import inspect
+
+def checkIfApacheIsRunning():
+	twitter = subprocess.Popen("service apache2 status", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	
+	#woops, lost connection to mothership!
+	if "is running" not in twitter.stdout.read():
+		cur_date = time.strftime("%x")
+		cur_time = time.strftime("%X")
+		tweet = "{0} - {1} - Apache is not running!".format(cur_date, cur_time)
+		twitter = subprocess.Popen("/usr/local/bin/twitter -edeadbird.fr@gmail.com set {0}".format(tweet), stderr=subprocess.PIPE, shell=True)
+
+
+if __name__ == '__main__':
+	checkIfApacheIsRunning()
--- update_check.py
+++ update_check.py
@@ -1,27 +1,41 @@
+# -*- coding: utf8 -*-
+
 import subprocess
 import time
+import inspect
 
+DEBUG_MODE = 0
 
+def checkForUpdate():
+	if DEBUG_MODE == 1:
+		print "entering {0}".format(inspect.stack()[0][3])
 
-#create a subprocess that launches apt-check and gathers results
-p = subprocess.Popen("/usr/lib/update-notifier/apt-check", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	#create a subprocess that launches apt-check and gathers results
+	p = subprocess.Popen('apt-get -s upgrade | grep -Po "^\\d+ (?=mis à jour)"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-#read stderr to a string
-str = p.stderr.read();
+	#read stderr to a string
+	str = p.stdout.read();
+	print "tjeriotjeroi {0}".format(str)
 
-#split it
-updates = int(str.split(";")[0])
-sec_updates = int(str.split(";")[1]) 
+	#split it
+	updates = int(str)
 
-#print "Number of packets that can be updated: {0}".format(updates)
-#print "Number of security update: {0}".format(sec_updates)
+	#if result is non zero, tweet it
+	if updates != 0 or sec_updates != 0:
+		#print "Twitting..."
+		cur_date = time.strftime("%x")
+		cur_time = time.strftime("%X")
+		tweet = "{0} - {1} - Your server has {2} updates pending!".format(cur_date, cur_time, updates)
+		twitter = subprocess.Popen("/usr/local/bin/twitter -edeadbird.fr@gmail.com set {0}".format(tweet), stderr=subprocess.PIPE, shell=True)
+	
+	#debug mode
+	if DEBUG_MODE == 1:
+		print "apt-check returned this: {0}".format(str)
+		print "Then I got this: updates={0}".format(updates)
+		#check for variable existence
+		if 'tweet' in locals():
+			print "Tweet = {0}".format(tweet)
+			print "Twitter subprocess returned this: {0}".format(twitter.stderr.read())
 
-#if result is non zero, tweet it
-if updates != 0 or sec_updates != 0:
-	#print "Twitting..."
-	cur_date = time.strftime("%x")
-	cur_time = time.strftime("%X")
-	tweet = "{0} - {1} - Your server has {2}+{3} updates pending!".format(cur_date, cur_time, updates, sec_updates)
-	p = subprocess.Popen("/usr/local/bin/twitter -edeadbird.fr@gmail.com set {0}".format(tweet), stderr=subprocess.PIPE, shell=True)
-	#out, err = p.communicate()
-	#print "Twitt sent!"
+if __name__ == '__main__':
+	checkForUpdate()
